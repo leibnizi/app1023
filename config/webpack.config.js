2e06868bf9a6c3f01a52e5868bfa2fc9bc133295
cruyff <cruyff@gmail.com>
2021-10-30 15:04:33 +0800

    add sass module

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -63,8 +63,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
-const lessRegex = /\.less$/;
-const lessModuleRegex = /\.module\.less$/;
+const lessRegex = /\\.(less)$/;
+const lessModuleRegex = /\\.module\\.less$/;
 
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === "true") {
@@ -511,6 +511,9 @@ module.exports = function (webpackEnv) {
                   sourceMap: isEnvProduction
                     ? shouldUseSourceMap
                     : isEnvDevelopment,
+                  modules: {
+                    getLocalIdent: getCSSModuleLocalIdent,
+                  },
                 },
                 "sass-loader"
               ),
@@ -545,20 +548,32 @@ module.exports = function (webpackEnv) {
               use: getStyleLoaders(
                 {
                   importLoaders: 2,
-                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                  sourceMap: isEnvProduction
+                    ? shouldUseSourceMap
+                    : isEnvDevelopment,
+                  modules: {
+                    getLocalIdent: getCSSModuleLocalIdent,
+                  },
                 },
                 "less-loader"
               ),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See <https://github.com/webpack/webpack/issues/6571>
               sideEffects: true,
             },
+            // Adds support for CSS Modules, but using LESS
+            // using the extension .module.less or .module.less
             {
               test: lessModuleRegex,
               use: getStyleLoaders(
                 {
                   importLoaders: 2,
                   sourceMap: isEnvProduction && shouldUseSourceMap,
-                  modules: true,
-                  getLocalIdent: getCSSModuleLocalIdent,
+                  modules: {
+                    getLocalIdent: getCSSModuleLocalIdent,
+                  },
                 },
                 "less-loader"
               ),
--- src/container/head/index.tsx
+++ src/container/head/index.tsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 
-// import "./styles.less";
-// import styles from "./styles.less";
-import "./styles.css";
+// import "./styles.scss";
+import styles from "./styles.scss";
+// import "./styles.css";
 
 export default class Head extends Component {
   render() {
     return (
       <div>
         <p className="top">head</p>
-        {/* <p className={styles.top}>head</p> */}
+        {/* <p className={styles.top2}>head</p> */}
+        <div className={styles.top}>toptoptoptop</div>
       </div>
     );
   }
